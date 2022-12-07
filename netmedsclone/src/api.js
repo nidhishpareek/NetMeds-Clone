223c8adb38022ba5381b8b10770a749261cafd0b
Gulshan7777 <sharmagulshan252@gmail.com>
2022-12-07 14:30:35 +0530

    deployment updated to render

--- netmedsclone/src/api.js
+++ netmedsclone/src/api.js
@@ -1,8 +1,9 @@
-export const API="https://netmeds-data.herokuapp.com"
-export const Product_API="https://netmeds-data.herokuapp.com/products"
-export const Cart_API="https://netmeds-data.herokuapp.com/cart"
-export const Home_API="https://netmeds-data.herokuapp.com/home"
-export const User_API="https://netmeds-data.herokuapp.com/user"
-export const SaveForLater_API="https://netmeds-data.herokuapp.com/saveForlater"
-export const Premium_API="https://netmeds-data.herokuapp.com/premiumMember"
+export const API="https://netmedsdata.onrender.com"
+export const Product_API="https://netmedsdata.onrender.com/products"
+export const Cart_API="https://netmedsdata.onrender.com/cart"
+export const Home_API="https://netmedsdata.onrender.com/home"
+export const User_API="https://netmedsdata.onrender.com/user"
+export const SaveForLater_API="https://netmedsdata.onrender.com/saveForlater"
+export const Premium_API="https://netmedsdata.onrender.com/premiumMember"
+// https://netmedsdata.onrender.com/
 
